6be41f177f178480fcf1994c164bf088046a23df
alenX <genhaoai@gmail.com>
2015-01-23 16:36:22 +0800

    add comments

--- GetChangedFilePath.py
+++ GetChangedFilePath.py
@@ -1,13 +1,12 @@
 # -*- coding:utf-8 -*-
 __author__ = 'wangss'
 import os
-import sys
 import time
 
 
 def getchangepath(pathlist, filetype):
     i = len(pathlist)
-    name = time.strftime('%Y-%m-%d-%H-%I-%M-%S', time.localtime(time.time()))
+    name = time.strftime('%Y-%m-%d %H-%I:%M:%S', time.localtime(time.time()))
     writefile = open('D:\\' + name + '.log', 'w')
     changefileamount = 0
     for j in range(i):
@@ -15,10 +14,10 @@ def getchangepath(pathlist, filetype):
         _type = filetype[j]
         for parent, dirnames, filenames in os.walk(path):
             for filename in filenames:
-                if filename.endswith(_type) and os.stat(os.path.join(parent, filename)).st_mode == 33206:
+                if filename.endswith(_type) and os.stat(os.path.join(parent, filename)).st_mode == 33206:  # 可写属性
                     name = "ChangedFile---" + os.path.join(parent, filename)
-                    writefile.write(name+'\n')
+                    writefile.write(name + '\n')
                     changefileamount += 1
     print '修改文件总数' + str(changefileamount)
-    writefile.write('修改文件总数' + str(changefileamount)+'\n')
+    writefile.write('修改文件总数' + str(changefileamount) + '\n')
     writefile.close()
